6f736b5c942ea1f6a3cfffa72bc1e41288a113cc
Maria Gloria Raquel Obono <obonoondo.m@northeastern.edu>
2023-02-25 19:43:01 -0600

    app

--- mysql/db.config.js
+++ mysql/db.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    HOST: "localhost",
+    HOST: "127.0.0.1",
     USER: "root",
     PASSWORD: "Gloria1996!",
     DB: "AWS-ec2",
+    PORT: "3306",
     dialect: "mysql",
     pool: {
       max: 5,

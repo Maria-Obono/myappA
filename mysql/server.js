ab18ce313583c2e3c3436cfd237a7d280cecba77
Maria Gloria Raquel Obono <obonoondo.m@northeastern.edu>
2023-03-02 06:33:36 -0600

    goog

--- mysql/server.js
+++ mysql/server.js
@@ -32,6 +32,19 @@ app.use(express.urlencoded({ extended: true }));
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to my application." });
+
+  const db = require("./app/models");
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+  db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });
 });
 
 require("./app/routes/User.routes")(app);
